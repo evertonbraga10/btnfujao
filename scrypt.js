18c3e5881c447aee81c7557d08ee6f25d08c6844
Everton Alves de Almeida Braga <evertonbraga.huwc@ebserh.gov.br>
2024-08-22 14:00:51 -0300

    arquivo-script

---
+++ scrypt.js
@@ -0,0 +1,14 @@
+let button = document.getElementById('no')
+        let height = window.innerHeight-50
+        let width = window.innerWidth-50
+
+        button.addEventListener('mouseover', function(){
+            button.style.position='absolute'
+            button.style.top=Math.random()*height+'px'
+            button.style.left=Math.random()*width+'px'
+        })
+
+        const btn = document.getElementById('yes')
+        btn.addEventListener('click', function(){
+            alert('Obrigado...')
+        })
